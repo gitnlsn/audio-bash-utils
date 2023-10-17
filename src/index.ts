f73a946f48119dae64a11c714df12d5ef6364b3f
nelson kenzo tamashiro <nelsonkenzotamashiro@gmail.com>
2023-10-17 20:06:18 -0300

    fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 export { execPromise } from "./execPromise";
+
 export { convertTimeToSeconds } from "./convertTimeToSeconds";
+export { parseSecondsIntoTime } from "./parseSecondsIntoTime";
+export { generateSegmentationMetadata } from "./generateSegmentationMetadata";
 
 export { mkTemp } from "./mkTemp";
 
-export { getAudioDuration } from "./getAudioDuration";
-export { convert } from "./convert";
-export { record } from "./record";
 export { validateFile } from "./validateFile";
+export { getAudioDuration } from "./getAudioDuration";
+export { segmentation } from "./segmentation";
+export { convert } from "./convert";
 export { rms } from "./rms";
 
+export { record } from "./record";
 export { filter } from "./filter";
 export { recordWithFilter } from "./recordWithFilter";
